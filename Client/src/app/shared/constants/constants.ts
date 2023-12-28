7c3fc07cf8e70f4fb742de381378c52a6a4f050d
jacko <jackowiakjacekbartek@gmail.com>
2023-12-28 19:43:58 +0100

    Podpięcie companypage do backendu, bez map/biur. Dodanie guzkikow edycji do companypage i wróć save do edit-companypage

--- Client/src/app/shared/constants/constants.ts
+++ Client/src/app/shared/constants/constants.ts
@@ -9,6 +9,7 @@ export const LANGUAGES = [
 
 // export const localUrl = 'http://localhost:5010'
 export const localUrl = 'http://192.168.56.1:5010' //your local IP
+export const localUrlCompanies = 'http://192.168.56.1:5013' //your local IP
 
 
 export const images = '/assets/images'
