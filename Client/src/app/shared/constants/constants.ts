cf2584ee44eb8b08b9aacc8090703c018914b990
Kacper W <kacper.wybierala1@gmail.com>
2024-01-13 21:09:09 +0100

    provide ip

--- Client/src/app/shared/constants/constants.ts
+++ Client/src/app/shared/constants/constants.ts
@@ -26,7 +26,7 @@ export enum JobType {
 
 // export const localUrl = 'http://localhost:5010'
 export const localUrl = 'http://68.219.240.80:5010' //your local IP
-export const comapniesUrl = 'http://localhost:5013' //your local company IP
+export const comapniesUrl = 'http://68.219.240.80:5013' //your local company IP
 export const quizzesUrl = 'http://68.219.240.80:5015/api/Quizzes' //your local company IP
 export const jobsUrl = 'http://68.219.240.80:5013' //your local company IP
 
