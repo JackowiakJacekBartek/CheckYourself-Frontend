59ce80398bee29d9fa847970d7a36b4e97f893a4
Kacper W <kacper.wybierala1@gmail.com>
2024-02-01 09:39:34 +0100

    fix result points counter

--- Client/src/app/pages/quizzes/result/result.component.ts
+++ Client/src/app/pages/quizzes/result/result.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResultService } from './result.service';
 import { ActivatedRoute } from '@angular/router';
 import { QuizResultDto } from 'src/app/shared/models/quizzes';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit {
   quizAttempt!: QuizResultDto;
   public resultId: number = this.route.snapshot.params['id'];
 
-  constructor(protected resultService: ResultService, private route: ActivatedRoute) {
+  constructor(protected resultService: ResultService, private route: ActivatedRoute) {}
 
-    resultService.getQuizAttemptResultById(this.resultId).subscribe(res => {
-      this.quizAttempt = res.methodResult;
-    });
+  ngOnInit() {
+    this.resultService.getQuizAttemptResultById(this.resultId)
+      .pipe(take(1))
+      .subscribe(res => {
+        this.quizAttempt = res.methodResult;
+        // this.logResults();
+      });
+  }
 
+  logResults() {
+    console.log(`Total Participant Score: ${this.quizAttempt.quizAttempt.totalparticipantscore}`);
+    console.log(`Total Score: ${this.quizAttempt.quiz.totalscore}`);
+  }
+
+  get resultMessage(): string {
+    return this.quizAttempt.quizAttempt.totalparticipantscore >= this.quizAttempt.quiz.totalscore!
+      ? 'Quiz.Passed'
+      : 'Quiz.Failed';
+  }
+
+  get resultColor(): string {
+    return this.quizAttempt.quizAttempt.totalparticipantscore >= this.quizAttempt.quiz.totalscore!
+      ? '#00A993'
+      : '#EB4335';
   }
 }
