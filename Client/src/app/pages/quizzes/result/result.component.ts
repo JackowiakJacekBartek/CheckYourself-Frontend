6190e9ce9918b5e9b7b3652782cd4c494a3b9270
Kacper W <kacper.wybierala1@gmail.com>
2024-02-01 09:49:01 +0100

    fix2 sorry

--- Client/src/app/pages/quizzes/result/result.component.ts
+++ Client/src/app/pages/quizzes/result/result.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ResultService } from './result.service';
 import { ActivatedRoute } from '@angular/router';
 import { QuizResultDto } from 'src/app/shared/models/quizzes';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-result',
@@ -10,13 +11,17 @@ import { QuizResultDto } from 'src/app/shared/models/quizzes';
 })
 export class ResultComponent {
   quizAttempt!: QuizResultDto;
+  examPassed!: boolean;
   public resultId: number = this.route.snapshot.params['id'];
 
-  constructor(protected resultService: ResultService, private route: ActivatedRoute) {
+  constructor(protected resultService: ResultService, private route: ActivatedRoute) {}
 
-    resultService.getQuizAttemptResultById(this.resultId).subscribe(res => {
-      this.quizAttempt = res.methodResult;
-    });
-
+  ngOnInit() {
+    this.resultService.getQuizAttemptResultById(this.resultId)
+      .pipe(take(1))
+      .subscribe(res => {
+        this.quizAttempt = res.methodResult;
+        this.examPassed = (this.quizAttempt.quizAttempt.totalparticipantscore / this.quizAttempt.quiz.totalscore!) * 100 >= this.quizAttempt.quiz.passingthreshold
+      });
   }
 }
